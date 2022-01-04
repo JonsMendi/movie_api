307fa779bae3ba76c4124c9bfdb96f19e5aa1a0a
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-04 14:25:39 +0100

    Updated index.js with new users, new movies list(in the future move to Database), new basic route endpoints for the movie and user and called a new function uuid and body-parser.
    If applied, this commit will have added three new users, new movies, called new functions (uuid and body-parser) and added new routes for all the endpoints with their handler methods, requests and responses.
    Was defined:
    - app.get('/users')(check all the users);
    -app.post('/users')(to create a user);
    -app.put('/users/:id')(to update the user name);
    -app.post('/users/:id/:movieTitle')(to add a movie to the profile);
    -app.delete('/users/:id/:movieTitle')(to remove a movie from the profile);
    -app.delete('/users/:id')(to delete the profile);
    -app.get('/movies')(to receive a object from all the movies);
    -app.get('/movies/:title')(to find a movie by the Title);
    -app.get('/movies/genre/:genre')(to find a Genre);
    -app.get('/movies/directors/:directorName')(to find a Director by name)

--- index.js
+++ index.js
@@ -1,91 +1,102 @@
 const express = require('express');
-    morgan = require('morgan');
-
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 const app = express();
 
-let topMovies = [
+let users = [
     {
-        Title: 'Raider\'s of the Lost Ark - Indiana Jones',
-        Year: 1981,
-        Genre: 'Adventure',
-        Director: 'Steven Spielberg',
-        Star: 'Harrison Ford'
+        id: 1,
+        name: "João",
+        favoriteMovies: []
     },
-
     {
-        Title: 'Schindler\'s List',
-        Year: 1993,
-        Genre: 'Biography',
-        Director: 'Steven Spielberg',
-        Star: 'Liam Neeson'
+        id: 2,
+        name: "Pedro",
+        favoriteMovies: []
     },
-
     {
-        Title: 'Catch Me If You Can',
-        Year: 2002,
-        Genre: 'Biography',
-        Director: 'Steven Spielberg',
-        Star: 'Leonardo DiCaprio'
-    },
-
-    {
-        Title: 'Empire of the Sun',
-        Year: 1987,
-        Genre: 'History',
-        Director: 'Steven Spielberg',
-        Star: 'Christian Bale'
-    },
-
-    {
-        Title: 'A.I. Artificial Intelligence',
-        Year: 2001,
-        Genre: 'Sci-Fi',
-        Director: 'Steven Spielberg',
-        Star: 'Haley Joel Osment'
-    },
-
-    {
-        Title: 'Jurassic Park',
-        Year: 1993,
-        Genre: 'Adventure',
-        Director: 'Steven Spielberg',
-        Star: 'Sam Neill'
-    },
-
-    {
-        Title: 'War of the Worlds',
-        Year: 2005,
-        Genre: 'Sci-Fi',
-        Director: 'Steven Spielberg',
-        Star: 'Tom Cruise'
-    },
-
-    {
-        Title: 'Saving Private Ryan',
-        Year: 1998,
-        Genre: 'War',
-        Director: 'Steven Spielberg',
-        Star: 'Tom Hanks'
-    },
-
-    {
-        Title: 'Jaws',
-        Year: 1975,
-        Genre: 'Thriller',
-        Director: 'Steven Spielberg',
-        Star: 'Roy Schneider'
-    },
-
-    {
-        Title: 'Close Encounters of the Third Kind',
-        Year: 1977,
-        Genre: 'Sci-Fi',
-        Director: 'Steven Spielberg',
-        Star: 'Richard Dreyfuss'
+        id: 3,
+        name: "Milton",
+        favoriteMovies: []
     },
 ]
 
-app.use(morgan('combined'));
+let topMovies = [
+    {
+        Title: 'The Shawshank Redemption',
+        Description: 'Two imprisoned men meet over several years, finding solace and eventual redemption through acts of common decency.',
+        Year: 1994,
+        Genre: 'Drama',
+        Director: {
+            Name: 'Frank Darabont',
+            Bio: 'Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution. Brought to America as an infant, he settled with his family in Los Angeles and attended Hollywood High School.',
+            Birth: 1959,
+            Death: 'Alive'
+        },
+        ImageUrl: 'https://images-na.ssl-images-amazon.com/images/I/81GHxM+oaaL._SX600_.jpg',
+        Star: 'Tim Robbins'
+    },
+    {
+        Title: 'The Godfather',
+        Description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+        Year: 1972,
+        Genre: 'Crime',
+        Director: {
+            Name: 'Francis Ford Coppola',
+            Bio: 'Francis Ford Coppola was born in 1939 in Detroit, Michigan, but grew up in a New York suburb in a creative, supportive Italian-American family. His father, Carmine Coppola, was a composer and musician. His mother, Italia Coppola (née Pennino), had been an actress. Francis Ford Coppola graduated with a degree in drama from Hofstra University, and did graduate work at UCLA in filmmaking.',
+            Birth: 1939,
+            Death: 'Alive'
+        },
+        ImageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61cqSr5PBxL._SX600_.jpg',
+        Star: 'Marlon Brando'
+    },
+    {
+        Title: 'Schindler\'s List',
+        Description: 'After witnessing the persecution of Jews in German-occupied Poland during World War II, industrialist Oskar Schindler begins to worry about his Jewish workforce.',
+        Year: 1993,
+        Genre: 'Drama',
+        Director: {
+            Name: 'Steven Spielberg',
+            Bio: 'One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood\'s best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Tubarão (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.',
+            Birth: 1946,
+            Death: 'Alive'
+        },
+        ImageUrl: 'https://m.media-amazon.com/images/I/6174SIM32yL._SY679_.jpg',
+        Star: 'Liam Neeson'
+    },
+    {
+        Title: 'Pulp Fiction',
+        Description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of gangsters intertwine in four stories of violence and redemption.',
+        Year: 1994,
+        Genre: 'Crime',
+        Director: {
+            Name: 'Quentin Tarantino',
+            Bio: 'Quentin Jerome Tarantino was born in Knoxville, Tennessee. His father, Tony Tarantino, is an Italian-American actor and musician from New York, and his mother, Connie (McHugh), is a nurse from Tennessee. Quentin moved with his mother to Torrance, California, when he was four years old.',
+            Birth: 1963,
+            Death: 'Alive'
+        },
+        ImageUrl: 'https://m.media-amazon.com/images/I/51ZXCKS8G8L.jpg',
+        Star: 'John Travolta'
+    },
+    {
+        Title: 'Fight Club',
+        Description: 'An office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much bigger.',
+        Year: 1999,
+        Genre: 'Drama',
+        Director: {
+            Name: 'David Fincher',
+            Bio: 'David Fincher was born in 1962 in Denver, Colorado, and was raised in Marin County, California. When he was 18 years old he went to work for John Korty at Korty Films in Mill Valley. He subsequently worked at ILM (Industrial Light and Magic) from 1981-1983. Fincher left ILM to direct TV commercials and music videos after signing with N. Lee Lacy in Hollywood.',
+            Birth: 1962,
+            Death: 'Alive'
+        },
+        ImageUrl: 'https://m.media-amazon.com/images/I/71V4mbT4n9L._SY679_.jpg',
+        Star: 'Brad Pitt'
+    },
+]
+
+app.use(bodyParser.json());
+app.use(morgan('common'));
 
 app.use(express.static('public'));
 
@@ -93,8 +104,120 @@ app.get('/', (req, res) => {
     res.send('Welcome to myMovie!');
 });
 
+//READ 'CRUD'
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+//CREATE 'CRUD'
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    if(newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser);
+    } else {
+        res.status(400).send('Sorry, user needs a name!')
+    }
+});
+
+//UPDATE 'CRUD'
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id );
+
+    if(user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('No name!');
+    }
+});
+
+//CREATE 'CRUD'
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if(user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`The ${movieTitle} as been added to ${users[0].name} profile`);
+    } else {
+        res.status(400).send('No movie with such name!');
+    }
+});
+
+//DELETE 'CRUD'
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if(user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`The ${movieTitle} as been removed from ${users[0].name} profile`);
+    } else {
+        res.status(400).send('No movie has been removed!');
+    }
+});
+
+//DELETE 'CRUD'
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if(user) {
+        users = users.filter( user => user.id != id);
+        res.status(200).send(`The user ${users[0].name} profile has been deleted`);
+    } else {
+        res.status(400).send('That user do not exist!');
+    }
+});
+
+//READ 'CRUD'
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.status(200).json(topMovies);
+});
+
+//READ 'CRUD'
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = topMovies.find(movies => movies.Title === title);
+
+        if (movie) {
+            res.status(200).json(movie);
+        } else {
+            res.status(400).send('That movie is not here sister!')
+        }
+});
+
+//READ 'CRUD'
+app.get('/movies/genre/:genre', (req, res) => {
+    const { genre } = req.params;
+    const genreName = topMovies.find(movies => movies.Genre === genre).Genre;//Just returning 'Drama' Instead of 'Genre': 'Drama'(Need to fix)
+
+        if (genre) {
+            res.status(200).json(genre);
+        } else {
+            res.status(400).send('That Genre is not here sister!')
+        }
+});
+
+//READ 'CRUD'
+app.get('/movies/directors/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = topMovies.find(movies => movies.Director.Name === directorName).Director;
+
+        if (director) {
+            res.status(200).json(director);
+        } else {
+            res.status(400).send('That Director is not here sister!')
+        }
 });
 
 app.use((err, req, res, next) => {
@@ -102,6 +225,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something is wrong sister!');
 });
 
-app.listen(8080, () => {
-    console.log('This app is listening on port 8080 brother.');
+app.listen(8088, () => {
+    console.log('This app is listening on port 8088 brother.');
 });
