15ec014bcc6201cc93114e5d6905b7ca14a95c97
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-01-14 16:54:39 +0100

    Updated with 'Brcrypt' creating a new hashPassword and validatePassword.
    If applied, this commit will have updated the models.js importing a bcrypt module that encrypts the password. This is called Hashing.
    Likewise, was added two new statements, one that generates the hashPassword (then used in index.js POST(creating a new user))and another that validatePassword inserted(used in passport.js in if condition).

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');//this module will be imported for 'Hashing' the password.
 
+//Movies collection Schema
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -21,6 +23,7 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+//User collection Schema
 let userSchema =  mongoose.Schema({
     Username: { type: String, required: true},
     Password: { type: String, required: true},
@@ -29,6 +32,11 @@ let userSchema =  mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+//Under bcrypt for Hashing
+userSchema.statics.hashPassword = (password) => { return bcrypt.hashSync(password, 10);}//this is called on index.js in the endpoint POST where Users can create a profile.
+userSchema.methods.validatePassword = function (password) { return bcrypt.compareSync(password, this.Password);};//this is called on passport.js in LocalStrategies to validate the password comparing with the one bcrypt when creating the password.
+
+//Actors collection Schema
 let actorSchema = mongoose.Schema({
     Name: {type: String, required: true},
     Bio: String,
