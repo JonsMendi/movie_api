06e0d35af9f032b4d7a1b790162f19982c1334ac
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-05-04 13:28:35 +0200

    added new allowedOrigins website

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Under to define who can have access to the API.
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:4200', 'https://jonsmendi.github.io/myFlix-Angular-client'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
