cd80d3a3eda17f3ae72107415a2b38c20e7afcf3
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-05-05 18:02:52 +0200

    addFavoriteMoves Change

--- index.js
+++ index.js
@@ -135,19 +135,16 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 
 //UPDATE 'CRUD' (Add movie in User's FavoriteMovies list)
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username }, {
-        $addToSet: { FavoriteMovies: req.params.MovieID }
-    },
-        { new: true }, // This line makes sure that the updated document is returned
-        (err, updatedUser) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error: ' + err);
-            } else {
-                res.send('The movie was successfully added to your list!');
-            }
-        });
-});
+    console.log(req.params.Username, req.params.MovieID)  
+    Users.findOneAndUpdate(
+      { Username: req.params.Username }, 
+      { $push: { FavouriteMovies: req.params.MovieID } },
+      { new: true }
+    ).then(u => {
+      res.json(u);
+    })
+    .catch(e => res.status(500).send('Error: ' + err))
+    });
 
 //DELETE 'CRUD' (Remove movie in User's FavoriteMovies list)
 app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
